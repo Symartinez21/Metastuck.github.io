a3ff94cb126d33834cdd2cbc1f9736846016ee09
Symartinez21 <sophia.martinez@sjsu.edu>
2020-04-15 17:18:01 -0700

    processing

---
+++ sketch_200415a/application.macosx/source/sketch_200415a.java
@@ -0,0 +1,75 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class sketch_200415a extends PApplet {
+
+PImage img;
+String message = "CORONAVIRUS";
+float x;
+float y;
+float hr, vr;
+float easing = 0.05f;
+
+  
+public void setup() {
+   
+  // Create the font
+  //textFont(createFont("SourceCodePro-Regular.ttf", 36));
+  textAlign(CENTER, CENTER);
+  
+  hr = textWidth(message) / 2;
+  vr = (textAscent() + textDescent()) / 2;
+  noStroke();
+  x = width / 2;
+  y = height / 2;
+  noStroke();  
+   img = loadImage("store.jpg");
+  
+}
+
+public void draw() { 
+  image(img, 0, 0);
+  
+  float targetX = mouseX;
+  float dx = targetX - x;
+  x += dx * easing;
+  
+  float targetY = mouseY;
+  float dy = targetY - y;
+  y += dy * easing;
+  
+  // Instead of clearing the background, fade it by drawing
+  // a semi-transparent rectangle on top
+  fill(204, 120);
+  rect(0, 0, width, height);
+  
+  // If the cursor is over the text, change the position
+  if (abs(mouseX - x) < hr &&
+      abs(mouseY - y) < vr) {
+    x += random(-5, 5);
+    y += random(-5, 5);
+  }
+  fill(0);
+  text("CORONAVIRUS", x, y);
+}
+  public void settings() {  size(1300, 866); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "sketch_200415a" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
